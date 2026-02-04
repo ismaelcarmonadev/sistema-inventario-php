1547ffbb070aec36ec816181fa6c295fe75b2729
Ismael Carmona <ismaelcarmonadev>
2026-02-03 21:32:46 -0600

    style: aplicacion de diseño en index

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php include("conexion.php"); ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
+<<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inventario Pro v2.0</title>
+    <title>Mi Inventario Pro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body class="bg-light">
 
